7ba9a8047ee7090ea7001bb0254d504634256377
Rahul sahani <153098636+Rahulsahani18@users.noreply.github.com>
2025-12-18 11:37:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://oswal.omsoftsolution.in',
+      "/api": {
+        target: "https://oswal.omsoftsolution.in",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/doctor/doctor/api'),
+        rewrite: (path) => path.replace(/^\/api/, "/doctor/doctor/api"),
         secure: false,
-      }
-    }
+      },
+    },
   },
-})
+});
